62d4be82544b1482a52f52168ca9df04883f004a
cduggan <craigduggan90@outlook.com>
2026-08-18 15:23:07 +0100

    feat: add toast primitive

---
+++ src/ui/src/components/Toast.tsx
@@ -0,0 +1,8 @@
+import { Toaster as BaseToaster } from '@/components/ui/sonner'
+import type { ToasterProps } from 'sonner'
+
+export { toast } from 'sonner'
+
+export function Toaster(props: ToasterProps) {
+  return <BaseToaster richColors {...props} />
+}
